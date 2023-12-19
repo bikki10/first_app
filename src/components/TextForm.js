386a52e4701e255e4a91e8c70a78b6e871db3e14
Bikki <bikrambhusal75@gmail.com>
2023-12-20 01:51:31 +0545

    Learned about hooks (useState), used event handling and created a program that converts given text into uppercase.

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const [text, setText] = useState("Enter Text here"); // array destructuring syntax
+  //   text = "new textsbdjs"; // Wrong way of changing the value of text variable
+  //   setText("new textsbjsb"); // Correct way of changing the value of text variable
+  const handleOnChange = (event) => {
+    // event is given as a parameter
+    console.log("on change");
+    setText(event.target.value); //Event handling syntax in JS
+  };
+  const handleUpClick = () => {
+    console.log("Text changed to UPPERCASE!");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
   return (
     <div>
       <div className="mb-3">
@@ -9,10 +22,13 @@ export default function TextForm(props) {
           className="form-control"
           id="exampleFormControlTextarea1"
           rows={12}
-          defaultValue={""}
+          value={text}
+          onChange={handleOnChange}
         />
       </div>
-      <button className="btn btn-primary">{props.button}</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        {props.button}
+      </button>
     </div>
   );
 }

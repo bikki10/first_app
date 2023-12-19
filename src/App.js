1fd6d6dd2f26b1b2fbf0a6d2f1fc8599b911d14a
Bikki <bikrambhusal75@gmail.com>
2023-12-19 23:59:48 +0545

    Props and PropsTypes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <Navbar title="TextUtils"/>
+      <Navbar title="TextUtils" home="Home" />
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
@@ -23,7 +24,7 @@ export default function Navbar(props) {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">
-                  Home
+                  {props.home}
                 </a>
               </li>
               <li className="nav-item">
@@ -85,3 +86,13 @@ export default function Navbar(props) {
     </div>
   );
 }
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+  home: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: "Title Here",
+  home: "Home button here",
+};

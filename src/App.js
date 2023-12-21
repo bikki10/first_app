7fb0048d315ce695bde365f92325f65cd6c35dbd
Bikki <bikrambhusal75@gmail.com>
2023-12-21 13:45:20 +0545

    Toggling dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" home="Home" />
       <div className="container my-3">
-        <TextForm heading="Hey there!" />
+        {/* <TextForm heading="Hey there!" /> */}
+        <About/>
       </div>
     </>
   );
